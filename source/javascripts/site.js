8b46cab600128deb126403380517ce5cfefe4c1c
Lucian Leustean <lucian.leustean@gmail.com>
2018-04-27 01:58:16 +0300

    Add animation for project details

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,23 @@
 // This is where it all goes :)
+console.log("js loaded ..");
+
+document.addEventListener("DOMContentLoaded", function() {
+  var projects = document.querySelectorAll('.list-inner-text');
+
+  for (var i = 0; i < projects.length; i++) {
+    projects[i].addEventListener('click', function(event) {
+      hideAllImages(projects)
+      event.currentTarget.parentElement.classList.add('active');
+      event.currentTarget.parentElement.querySelectorAll('.project-details')[0].classList.remove('hide');
+    });
+  }
+});
+
+function hideAllImages(projects) {
+  for (var i = 0; i < projects.length; i++) {
+    if (projects) {
+      projects[i].parentElement.classList.remove('active');
+      projects[i].parentElement.querySelectorAll('.project-details')[0].classList.add('hide');
+    }
+  }
+}
